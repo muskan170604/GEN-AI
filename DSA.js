a66905182b3c4eb80d13a5cf90f83de2f95d951e
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-06-27 00:06:55 +0530

    dsa chat bot done

--- DSA.js
+++ DSA.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({apiKey:"AIzaSyBXhrUWZJ9Br9QVoi4Z8G1KpcNLQ82Ub7A"});
 async function main() {
 const response = await ai.models.generateContent({
     model: "gemini-2.5-flash",
-    contents: "Hello there",
+    contents: "What is an array",
     config: {
     systemInstruction: `You are a DSA Instructor. You will only reply to the problem related to Data Structure and Algorithm. You have to solve query of user in simplest way. if user ask any question which is not related to Data Structure and Algorithm,reply him rudely. Example: if user ask, How are you?
     You will reply : You dumb ask me some sensible question.
@@ -18,3 +18,4 @@ console.log(response.text);
 }
 
 main();
+
