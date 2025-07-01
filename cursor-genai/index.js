b05b01cf605c2199542d2d41422631e18435feb0
muskan170604 <muskan_ug_23@civil.nits.ac.in>
2025-07-02 00:29:42 +0530

    setup done

---
+++ cursor-genai/index.js
@@ -0,0 +1,164 @@
+import { GoogleGenAI } from "@google/genai";
+import readlineSync from 'readline-sync';
+import {exec} from "child_process";
+import {promisify} from "util";
+import os from "os";
+
+const platform=os.platform();
+
+const asyncExecute=promisify(exec);
+
+
+const History = [];
+
+const ai = new GoogleGenAI({ apiKey: "AIzaSyBXhrUWZJ9Br9QVoi4Z8G1KpcNLQ82Ub7A" });
+
+
+//Tool create karte h , jo kisi bhi terminal/shell command ko execute kar sakta h
+
+async function executeCommand({command}){
+
+    try{
+    const {stdout,stderr}=exec(command);
+    if(stderr){
+        return `Error:${stderr}`
+    }
+
+    return `Success: ${stdout} || Task executed completely`
+    }
+    catch(error){
+        return `Error: ${error}`
+    }
+}
+
+
+const executeCommandDeclaration={
+    name:"executeCommand",
+    description:"Execute a single terminal/shell command. A command can be to create a folder, file,write on a file,edit the file or delete the file",
+    parameters:{
+        type:'OBJECT',
+        properties:{
+            command:{
+                type:'STRING',
+                description: 'It will be a single terminal command. Ex: "mkdir calculator'
+            },
+        },
+        required:['command']
+    }
+}
+
+
+const availableTools = {
+    executeCommand
+}
+
+
+
+async function runAgent(userProblem) {
+
+    History.push({
+        role:'user',
+        parts:[{text:userProblem}]
+    });
+
+    
+    while(true){
+    
+    const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: History,
+    config: {
+        systemInstruction: `You are an website builder expert. You have to create the frontend of the website by analysing the user input.You have acess of tools, which can run or execute any shell or terminal command.
+        
+        Current user operation system is: ${platform}
+        Give command to the user according to its operating system support.
+
+        <-- What is your job -->
+        1.Analyze thw user query to see what type of website the want to build
+        2. Give them command one by one , step by step
+        3.Use available tool executeCommand
+
+        //Now you can give them command in following below
+        1. First create a folder, Ex:mkdir "calculator"
+        2.OInside the folder,create index.html Ex: touch "calculator/index.html"
+        3.Then create style.css same as above
+        4. Then create script.js
+        5. Then write a code in html file
+
+        You have to provide the terminal or shell command to the user they will directly execute it.
+        `,
+    tools: [{
+        functionDeclarations: [executeCommandDeclaration]
+    }],
+    },
+    });
+
+
+    if(response.functionCalls&&response.functionCalls.length>0){
+    
+    console.log(response.functionCalls[0]);
+    const {name,args} = response.functionCalls[0];
+
+    const funCall =  availableTools[name];
+    const result = await funCall(args);
+
+    const functionResponsePart = {
+        name: name,
+        response: {
+        result: result,
+        },
+    };
+
+    // model
+    History.push({
+        role: "model",
+        parts: [
+        {
+            functionCall: response.functionCalls[0],
+        },
+        ],
+    });
+
+    // result Ko history daalna
+
+    History.push({
+        role: "user",
+        parts: [
+        {
+            functionResponse: functionResponsePart,
+        },
+        ],
+    });
+    }
+    else{
+
+    History.push({
+        role:'model',
+        parts:[{text:response.text}]
+    })
+    console.log(response.text);
+    break;
+    }
+
+
+    }
+
+
+
+
+}
+
+
+async function main() {
+    const userProblem = readlineSync.question("Ask me anything--> ");
+    await runAgent(userProblem);
+    main();
+}
+
+
+main();
+
+
+
+
+
